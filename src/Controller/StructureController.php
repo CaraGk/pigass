f0f1c0bec46c51f63a5ed152540ec03c571dd31c
Pierre-François Pilou Angrand <caragk@medlibre.fr>
2020-08-10 23:10:28 +0200

    Permet aux Student non member de se connecter

--- src/Controller/StructureController.php
+++ src/Controller/StructureController.php
@@ -60,6 +60,8 @@ class StructureController extends AbstractController
                 $last_membership = $this->em->getRepository('App:Membership')->getLastForPerson($person);
                 $current_membership = $this->em->getRepository('App:Membership')->getCurrentForPerson($person);
                 if (!$last_membership) {
+                    $slug = $person->getStructure()->getSlug();
+                    $this->session->set('slug', $slug);
                     return $this->redirect($this->generateUrl('app_structure_map'));
                 } else {
                     $slug = $last_membership->getStructure()->getSlug();
